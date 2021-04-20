046e6affe75199722829e8980e9517cc16922f9f
Kate <kate.kovalyova.ua@gmail.com>
2021-04-20 10:56:44 +0300

    prompt movies object

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hi');
+const numberOfFilms = prompt ('How many films have you already watched?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt ('What is the last movie you watched?', ''),
+      b = prompt ('How would you rate it?', ''),
+      c = prompt ('What is the last movie you watched?', ''),
+      d = prompt ('How would you rate it?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
